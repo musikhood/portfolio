7ecd13821d2b8e96b04d0fc0eb35fb27c37bfcf5
Rafał Białowski <b.bialowski@gmail.com>
2021-11-29 22:04:56 +0100

    Add animation scroll

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector(".section__projects").addEventListener("click",function(){
+    document.querySelector(".projekty").scrollIntoView({ behavior: 'smooth', block: 'start'})
+})
+document.querySelector(".li1").addEventListener("click",function(){
+    document.querySelector(".about").scrollIntoView({ behavior: 'smooth', block: 'start'})
+})
+document.querySelector(".li2").addEventListener("click",function(){
+    document.querySelector(".projekty").scrollIntoView({ behavior: 'smooth', block: 'start'})
+})
+document.querySelector(".li3").addEventListener("click",function(){
+    document.querySelector(".kontakt").scrollIntoView({ behavior: 'smooth', block: 'start'})
+})
